4eab851b4f6ef49482b1d12fbb6d7d7155dcd09c
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2023-10-08 12:18:58 +0700

    Thêm api cho MetaTools

--- src/MetaTools/App.xaml.cs
+++ src/MetaTools/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using MetaTools.ViewModels;
+﻿using DryIoc;
+using MetaTools.ViewModels;
 using MetaTools.Views;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Prism.Ioc;
 using System.Windows;
 
@@ -10,6 +14,35 @@ namespace MetaTools
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction() || app.Environment.IsStaging())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.RunAsync();
+        }
+
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
---
+++ src/MetaTools/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetaTools.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet("GET")]
+    public IActionResult Get()
+    {
+        return Ok("OK");
+    }
+}
