f2a3595b551b5586eeffc0d13931f804b9b7d52c
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2023-10-11 00:43:42 +0700

    Add account controller

---
+++ src/MetaTools/Controllers/AccountInfoController.cs
@@ -0,0 +1,22 @@
+﻿using MetaTools.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetaTools.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountInfoController : ControllerBase
+    {
+        private readonly IAccountInfoRepository _accountInfoRepository;
+
+        public AccountInfoController(IAccountInfoRepository accountInfoRepository)
+        {
+            _accountInfoRepository = accountInfoRepository;
+        }
+        [HttpGet("gets")]
+        public IActionResult GetAll()
+        {
+            return Ok(_accountInfoRepository.GetAllAccountsAsync());
+        }
+    }
+}
