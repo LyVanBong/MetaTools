334de1a2a7acacd0d9228b21951cbd12edfe4fad
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2023-10-16 23:21:55 +0700

    Set Country Code

--- src/MetaTools/App.xaml.cs
+++ src/MetaTools/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MetaTools.Services.Facebook;
 using MetaTools.Services.TwoFactorAuthentication;
+using System.Globalization;
 
 namespace MetaTools
 {
@@ -8,20 +9,23 @@ namespace MetaTools
     /// </summary>
     public partial class App
     {
-        protected override  void OnStartup(StartupEventArgs e)
+        protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
             // Config AppCenter
+            var countryCode = RegionInfo.CurrentRegion.TwoLetterISORegionName;
+            AppCenter.SetCountryCode(countryCode);
+
             AppCenter.Start("1eaba9dd-3cb4-40c0-8757-124b3b247488",
                 typeof(Analytics), typeof(Crashes));
+            
+            AppCenter.SetEnabledAsync(true);
+            Crashes.SetEnabledAsync(true);
+            Analytics.SetEnabledAsync(true);
 
-             AppCenter.SetEnabledAsync(true);
-             Crashes.SetEnabledAsync(true);
-             Analytics.SetEnabledAsync(true);
-
-             // Định danh app
-           var installId = AppCenter.GetInstallIdAsync();
+            // Định danh app
+            //var installId = AppCenter.GetInstallIdAsync();
 
             // Tự đặt id
             //AppCenter.SetUserId("your-user-id");
